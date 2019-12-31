7bc1fb98e7dbb63ca3a90198d6a45abc3f747951
Timas Jackus <timas@mediapark.com>
2019-12-31 16:45:31 +0200

    update

--- flpenum.cpp
+++ flpenum.cpp
@@ -4,13 +4,14 @@
 #include <math.h>
 #include <time.h>
 #include <sys/time.h>
+#include <algorithm>
+#include <mpi.h>
 
 using namespace std;
 
 int numDP = 1000;      // Vietoviu skaicius (demand points, max 10000)
 int numPF = 10;        // Esanciu objektu skaicius (preexisting facilities)
-int numCL = 25;        // Kandidatu naujiems objektams skaicius (candidate locations)
-int numX  = 5;         // Nauju objektu skaicius
+int numCL = 26;        // Kandidatu naujiems objektams skaicius (candidate locations)
 
 double **demandPoints; // Geografiniai duomenys
 
@@ -20,47 +21,103 @@ double **demandPoints; // Geografiniai duomenys
 double getTime();
 void loadDemandPoints();
 double HaversineDistance(double* a, double* b);
-double evaluateSolution(int *X);
-int increaseX(int *X, int index, int maxindex);
+double evaluateSolution(int *X, int numX);
+int increaseX(int *X, int index, int maxindex, int numX);
+int calculateCombinationsCount(int numX);
+void calculateValues(int numX, int rank, int comm_size, double ts);
 
 //=============================================================================
 
-int main() {
-	
-	double ts = getTime();          // Algoritmo vykdymo pradzios laikas
+int main(int argc, char *argv[]) {
+	double ts = getTime();
+	ts = getTime();
+	int rank, comm_size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
 
-	loadDemandPoints();             // Nuskaitomi duomenys
-	
-	// Sudarom pradini sprendini: [0, 1, 2, 3, ...]
+	loadDemandPoints();
+
+	for (int numX = 3; numX <= 5; numX++) {
+		calculateValues(numX, rank, comm_size, ts);
+		MPI_Barrier(MPI_COMM_WORLD);
+	}
+
+    MPI_Finalize();
+	return 0;
+}
+
+void calculateValues(int numX, int rank, int comm_size, double ts) {
+	int combinationCount = calculateCombinationsCount(numX);
 	int *X = new int[numX];
-	int *bestX = new int[numX];
+	int bestX[numX];
 	for (int i=0; i<numX; i++) {
 		X[i] = i;
 		bestX[i] = i;
 	}
-	double u = evaluateSolution(X);
+	double u = evaluateSolution(X, numX);
 	double bestU = u;
 	int r;
+
 	//----- Pagrindinis ciklas ------------------------------------------------
+	int i = 0;
 
 	while (true) {
-		if (increaseX(X, numX-1, numCL)) {
-			u = evaluateSolution(X);
-			if (u > bestU) {
-				bestU = u;
-				for (int i=0; i<numX; i++) bestX[i] = X[i];
+		i++;
+		if (increaseX(X, numX-1, numCL, numX)) {
+			if (i < (combinationCount / comm_size) * (rank + 1) && i > (combinationCount / comm_size) * (rank)) {
+					u = evaluateSolution(X, numX);
+					if (u > bestU) {
+						bestU = u;
+						for (int i=0; i<numX; i++) bestX[i] = X[i];
+					}
+			} else if(i >= (combinationCount / comm_size) * (rank + 1)) {
+				break;
 			}
 		}
 		else break;
 	}
 	//----- Rezultatu spausdinimas --------------------------------------------
-	
-	double tf = getTime();     // Skaiciavimu pabaigos laikas
 
-	cout << "Geriausias sprendinys: ";
-	for (int i=0; i<numX; i++) cout << bestX[i] << " ";
-	cout << "(" << bestU << ")" << endl;
-	cout << "Skaiciavimo trukme: " << tf-ts << endl;
+	MPI_Barrier(MPI_COMM_WORLD);
+
+    double *receivedBestU = (double *)malloc(sizeof(double) * comm_size);
+	int receivedBestX[comm_size][numX];
+
+    MPI_Gather(&bestU, 1, MPI_DOUBLE, receivedBestU, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Gather(&bestX, numX, MPI_INT, &receivedBestX, numX, MPI_INT, 0, MPI_COMM_WORLD);
+
+	if (rank == 0) {
+		int bU = 0;
+		int bI = 0;
+
+		for (int i = 0; i < comm_size; i++) {
+			if (receivedBestU[i] > bU) {
+				bI = i;
+				bU = receivedBestU[i];
+			}
+		}
+	
+		double tf = getTime();     // Skaiciavimu pabaigos laikas
+		cout << "numX: " << numX << endl;
+		cout << "Geriausias sprendinys: ";
+		for (int i=0; i<numX; i++) cout << receivedBestX[bI][i] << " ";
+		cout << "(" << bU << ")" << endl;
+		cout << "Skaiciavimo trukme: " << tf-ts << endl;
+	}
+}
+
+//-
+
+int calculateCombinationsCount(int numX) {
+	int count = numCL;
+	int count2 = numX;
+	for (int i = 1; i < numX; i++) {
+		count *= numCL - i;
+		count2 *= numX - i;
+	}
+
+	return count / count2;
 }
 
 //=============================================================================
@@ -98,7 +155,7 @@ double getTime() {
 
 //=============================================================================
 
-double evaluateSolution(int *X) {
+double evaluateSolution(int *X, int numX) {
 	double U = 0;
 	int bestPF;
 	int bestX;
@@ -122,7 +179,7 @@ double evaluateSolution(int *X) {
 
 //=============================================================================
 
-int increaseX(int *X, int index, int maxindex) {
+int increaseX(int *X, int index, int maxindex, int numX) {
 	if (X[index]+1 < maxindex-(numX-index-1)) {
 		X[index]++;
 	}
@@ -131,7 +188,7 @@ int increaseX(int *X, int index, int maxindex) {
 			return 0;
 		}
 		else {
-			if (increaseX(X, index-1, maxindex)) X[index] = X[index-1]+1;
+			if (increaseX(X, index-1, maxindex, numX)) X[index] = X[index-1]+1;
 			else return 0;
 		}	
 	}
